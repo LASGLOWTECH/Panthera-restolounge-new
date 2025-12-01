000fe5e1768ad59179d4dbcb00fd2e6f4074e8f2
LASGLOWTECH <omozemojeau@gmail.com>
2025-12-01 12:24:06 +0100

    added logout details

--- app/admin/login/page.jsx
+++ app/admin/login/page.jsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import sideImage  from "@/public/assets/log.jpg";
-
+import { FaArrowLeftLong } from "react-icons/fa6";
 export default function AdminLoginPage() {
   const [email, setEmail] = useState("");  // this replaces username
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
-
+ 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
@@ -36,9 +36,22 @@ export default function AdminLoginPage() {
 
   return (
     <div className="min-h-screen flex">
+
+
+      
       {/* LEFT SIDE — Form */}
       <div className="w-full md:w-1/2 flex items-center justify-center bg-black p-10">
+     
+       
+      
         <div className="w-full max-w-md bg-dark p-6 mt-16 rounded-2xl">
+           <button
+          onClick={() => router.push('/')}
+          className="flex  text-white hover:text-white text-lg font-semibold"
+        >
+          <FaArrowLeftLong className="mr-2" size={20} />
+          Back
+        </button>
           <h2 className="text-4xl font-bold my-6 font-serif text-gold">Log into admin</h2>
           <p className="text-gray-400 mb-6">Manage events and reservation requests</p>
           <form onSubmit={handleSubmit} className="space-y-4">
